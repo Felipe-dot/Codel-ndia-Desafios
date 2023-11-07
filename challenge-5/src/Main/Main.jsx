64e0a6aab92ea9d7cf9f6c53be9f63fe3596bf90
Felipe-dot <felipeluis610@gmail.com>
2023-11-07 12:00:02 -0300

    :lipstick: Add Main component

---
+++ challenge-5/src/Main/Main.jsx
@@ -0,0 +1,32 @@
+import "./Main.css";
+
+export default function Main() {
+  return (
+    <>
+      <div className="main-container">
+        <div className="content-left">
+          <h3>HAYAO MIYAZAKI</h3>
+          <h1>A VIAGEM DE CHIHIRO</h1>
+          <h4>
+            Chihiro chega a um mundo mágico dominado por uma bruxa. Aqueles que
+            a desobedecem são transformados em animais.
+          </h4>
+          <div className="buttons">
+            <button>
+              <p>
+                <img src="src/assets/play.svg" alt="butão de reproduzir" />
+                ASSISTIR AGORA
+              </p>
+            </button>
+            <button>
+              <p>ASSITA O TRAILER</p>
+            </button>
+          </div>
+        </div>
+        <div className="content-right">
+          <img src="src/assets/image.svg" alt="fantasmas do chichiro " />
+        </div>
+      </div>
+    </>
+  );
+}
